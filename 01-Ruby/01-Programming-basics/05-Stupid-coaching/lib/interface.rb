2cdf556f87817d1c59c77f8b098a12bd604494ff
claire demont <claire.edith.demont@gmail.com>
2020-10-01 12:49:14 +0200

    done but relly not official or best answer

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,11 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "What do you want to say to coach?"
+my_line = gets.chomp
+
+
+while my_line!= "I am going to work right now!" && my_line != "I AM GOING TO WORK RIGHT NOW!"
+  puts coach_answer_enhanced(my_line)
+  puts "What do you want to say to coach?"
+  my_line = gets.chomp
+end
